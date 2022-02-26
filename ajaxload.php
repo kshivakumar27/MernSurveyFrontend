697bd87c78f2e92ce9dbca2eae8c302278a32305
Shivakumar K <56855151+kshivakumar27@users.noreply.github.com>
2022-02-26 10:45:54 +0530

    file committed

---
+++ ajaxload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#div1").load("prc.txt");
+  });
+});
+</script>
+</head>
+<body>
+
+<div id="div1"><h2>Let jQuery AJAX Change This Text</h2></div>
+
+<button>Get External Content</button>
+
+</body>
+</html>
